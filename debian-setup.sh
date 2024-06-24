e1a79e45a2843b15be211bb379e0568fa14dfaa8
David Vogel <david@davidvogel.xyz>
2024-06-23 20:43:17 -0400

    whiptail update
    changes in this commit include:
    - refactored "debian-setup.sh" in order to use "whiptail" dialog boxes
    - added a "packages.csv" file in order to streamline adding and removing packages
    - updated the README

--- debian-setup.sh
+++ debian-setup.sh
@@ -4,230 +4,154 @@
 # VARIABLES
 ################################
 
-additionalpkgs=(
-"curl"
-"git"
-"gpg"
-"htop"
-"lvm2"
-"rsync"
-"ssh"
-"stow"
-"sudo"
-"tmux"
-"tree"
-"vim"
-)
-
-basepkgs=(
-"linux-image-generic"
-"build-essential"
-"network-manager"
-)
-
-cryptanswers=(
-"Yes"
-"No"
-"I don't know"
-)
-
-firmwareanswers=(
-"Legacy BIOS"
-"UEFI"
-"I don't know"
-)
-
 localeconf=(
 'export LANG="en_US.UTF-8"
 export LC_COLLATE="C"'
 )
 
-regions=(
-"en_US"
-)
+packfile="https://raw.githubusercontent.com/DavidVogelxyz/debian-setup/master/packages.csv"
 
-swapanswers=(
-"Yes"
-"No"
-"I don't know"
-)
-
-timezones=(
-"US/Eastern"
-"US/Central"
-"US/Mountain"
-"US/Pacific"
-)
+TERM=linux
 
 ################################
 # FUNCTIONS
 ################################
 
-getuserandpass() {
-    echo "Please enter a password for the root user."
-    read -sp "Root password: " rootpass1
+error() {
+    # Log to stderr and exit with failure.
+    printf "%s\n" "$1" >&2
+    exit 1
+}
 
-    echo -e "\\nPlease retype the password."
-    read -sp "Verify: " rootpass2
+getuserandpass() {
+    rootpass1=$(whiptail --title "Root Password" --passwordbox "\\nPlease enter a password for the root user." \
+        --nocancel 10 60 3>&1 1>&2 2>&3 3>&1
+    )
+
+    rootpass2=$(whiptail --title "Root Password" --passwordbox "\\nPlease retype the password for the root user." \
+        --nocancel 10 60 3>&1 1>&2 2>&3 3>&1
+    )
 
     while ! [ "$rootpass1" = "$rootpass2" ]; do
-        echo -e "\\nThe passwords entered do not match each other.\\n\\nPlease enter the passwords again."
-        read -sp "Root password: " rootpass1
+        rootpass1=$(whiptail --title "Root Password" --passwordbox "\\nThe passwords entered do not match each other.\\n\\nPlease enter the root user's password again." \
+            --nocancel 10 60 3>&1 1>&2 2>&3 3>&1
+        )
 
-        echo -e "\\nPlease retype the password."
-        read -sp "Verify: " rootpass2
+        rootpass2=$(whiptail --title "Root Password" --passwordbox "\\nPlease retype the password for the root user." \
+            --nocancel 10 60 3>&1 1>&2 2>&3 3>&1
+        )
     done
 
-    echo -e "\\n\\nPlease enter a name for the new user that will be created by the script."
-    read -p "Username: " username
+    username=$(whiptail --title "Username" --inputbox "\\nPlease enter a name for the new user that will be created by the script." \
+        10 60 3>&1 1>&2 2>&3 3>&1
+    ) || exit 1
 
     while ! echo "$username" | grep -q "^[a-z][a-z0-9_-]*$"; do
-        echo "Invalid username. Please provide a username using lowercase letters; numbers, -, or _ can be used if not the first character."
-        read -p "Username: " username
+        username=$(whiptail --title "Username" --inputbox "\\nInvalid username. Please provide a username using lowercase letters; numbers, -, or _ can be used if not the first character." \
+            --nocancel 10 60 3>&1 1>&2 2>&3 3>&1
+        )
     done
 
-    echo -e "\\nPlease enter a password for the new user."
-    read -sp "User password: " userpass1
+    userpass1=$(whiptail --title "User Password" --passwordbox "\\nPlease enter a password for $username." \
+        --nocancel 10 60 3>&1 1>&2 2>&3 3>&1
+    )
 
-    echo -e "\\nPlease retype the password."
-    read -sp "Verify: " userpass2
+    userpass2=$(whiptail --title "User Password" --passwordbox "\\nPlease retype password for $username." \
+        --nocancel 10 60 3>&1 1>&2 2>&3 3>&1
+    )
 
     while ! [ "$userpass1" = "$userpass2" ]; do
-        echo -e "\\nThe passwords entered do not match each other.\\n\\nPlease enter the passwords again."
-        read -sp "User password: " userpass1
+        userpass1=$(whiptail --title "User Password" --passwordbox "\\nThe passwords entered do not match each other.\\n\\nPlease enter $username's password again." \
+            --nocancel 10 60 3>&1 1>&2 2>&3 3>&1
+        )
 
-        echo -e "\\nPlease retype the password."
-        read -sp "Verify: " userpass2
+        userpass2=$(whiptail --title "User Password" --passwordbox "\\nPlease retype password for $username." \
+            --nocancel 10 60 3>&1 1>&2 2>&3 3>&1
+        )
     done
 }
 
 getnetworkinginfo() {
-    echo -e "\\n\\nPlease enter a hostname for the Debian computer."
-    read -p "Hostname: " hostname
+    hostname=$(whiptail --title "Hostname" --inputbox "\\nPlease enter a hostname for the Debian computer." \
+        10 60 3>&1 1>&2 2>&3 3>&1
+    ) || exit 1
 
     while ! echo "$hostname" | grep -q "^[a-z][a-z0-9_-]*$"; do
-        echo "Invalid hostname. Please provide a hostname using lowercase letters; numbers, -, or _ can be used if not the first character."
-        read -p "Hostname: " hostname
+        hostname=$(whiptail --title "Hostname" --inputbox "\\nInvalid hostname. Please provide a hostname using lowercase letters; numbers, -, or _ can be used if not the first character." \
+            10 60 3>&1 1>&2 2>&3 3>&1
+        )
     done
 
-    echo -e "\\nPlease enter the domain of the network. If unsure, just enter 'local'."
-    read -p "Local domain: " localdomain
+    localdomain=$(whiptail --title "Local Domain" --inputbox "\\nPlease enter the domain of the network. If unsure, just enter 'local'." \
+        10 60 3>&1 1>&2 2>&3 3>&1
+    ) || exit 1
 
     while ! echo "$localdomain" | grep -q "^[a-z][a-z0-9_.-]*$"; do
-        echo "Invalid domain. Please provide a domain using lowercase letters; numbers, -, _, or . can be used if not the first character."
-        read -p "Local domain: " localdomain
+        localdomain=$(whiptail --title "Local Domain" --inputbox "\\nInvalid domain. Please provide a domain using lowercase letters; numbers, -, _, or . can be used if not the first character." \
+            10 60 3>&1 1>&2 2>&3 3>&1
+        )
     done
 }
 
 questions() {
-    # timezone
-    echo -e "\\nWhat timezone are you in?"
-    PS3="Please choose a number: "
+    timezone=$(whiptail --title "Timezone" --menu "\\nWhat timezone are you in?" \
+        14 60 4 \
+        "US/Eastern" ""\
+        "US/Central" ""\
+        "US/Mountain" ""\
+        "US/Pacific" ""\
+        3>&1 1>&2 2>&3 3>&1
+    )
 
-    select timezone in "${timezones[@]}"; do
-        case $REPLY in
-            1) timezone="US/Eastern"; break;;
-            2) timezone="US/Central"; break;;
-            3) timezone="US/Mountain" ; break;;
-            4) timezone="US/Pacific" ; break;;
-            *) echo "Unknown response. Try again.";;
-        esac
-    done
+    region=$(whiptail --title "Region" --menu "\\nWhat region are you in?" \
+        14 60 4 \
+        "en_US" ""\
+        3>&1 1>&2 2>&3 3>&1
+    )
 
-    echo "Selected '$timezone'"
+    firmwareanswer=$(whiptail --title "Firmware" --menu "\\nIs this computer running 'legacy BIOS' or 'UEFI'?" \
+        14 80 4 \
+        "BIOS" ""\
+        "UEFI" ""\
+        3>&1 1>&2 2>&3 3>&1
+    ) || exit 1
 
-    # region
-    echo -e "\\nWhat region are you in?"
-    PS3="Please choose a number: "
-
-    select region in "${regions[@]}"; do
-        case $REPLY in
-            1) region="en_US"; break;;
-            *) echo "Unknown response. Try again.";;
-        esac
-    done
-
-    echo "Selected '$region'"
-
-    # BIOS or UEFI?
-    echo -e "\\nIs this computer running 'legacy BIOS' or 'UEFI'?"
-    PS3="Please choose a number: "
-
-    select firmwareanswer in "${firmwareanswers[@]}"; do
-        case $REPLY in
-            1) firmwareanswer="BIOS"; break;;
-            2) firmwareanswer="UEFI"; break;;
-            3) echo "Confirm and come back."; exit 1;;
-            *) echo "Unknown response. Try again.";;
-        esac
-    done
-
-    echo "Selected '$firmwareanswer'"
+    sdx="Ignore since UEFI"
 
     [[ $firmwareanswer == "BIOS" ]] && {
-        echo
-        read -p "What is the device name (ex. sda, sdb, nvme0n1)? " sdx
+        sdx=$(whiptail --title "Device Name" --inputbox "\\nWhat is the device name (ex. sda, sdb, nvme0n1)?" \
+            10 60 3>&1 1>&2 2>&3 3>&1
+        )
     }
 
-    # encrypted?
-    echo -e "\\nIs this computer's root storage encrypted?"
-    PS3="Please choose a number: "
+    cryptanswer=$(whiptail --title "Encrypted System?" --menu "\\nIs this computer's root storage encrypted?" \
+        14 80 4 \
+        "yes" ""\
+        "no" ""\
+        3>&1 1>&2 2>&3 3>&1
+    ) || exit 1
 
-    select cryptanswer in "${cryptanswers[@]}"; do
-        case $REPLY in
-            1) cryptanswer="yes"; break;;
-            2) cryptanswer="no"; break;;
-            3) echo "Confirm and come back."; exit 1;;
-            *) echo "Unknown response. Try again.";;
-        esac
-    done
-
-    echo "Selected '$cryptanswer'"
-
-    # swap?
-    echo -e "\\nDoes this computer have a swap partition?"
-    PS3="Please choose a number: "
-
-    select swapanswer in "${swapanswers[@]}"; do
-        case $REPLY in
-            1) swapanswer="yes"; break;;
-            2) swapanswer="no"; break;;
-            3) echo "Confirm and come back."; exit 1;;
-            *) echo "Unknown response. Try again.";;
-        esac
-    done
-
-    echo "Selected '$swapanswer'"
+    swapanswer=$(whiptail --title "Swap Partition?" --menu "\\nDoes this computer have a swap partition?" \
+        14 80 4 \
+        "yes" ""\
+        "no" ""\
+        3>&1 1>&2 2>&3 3>&1
+    ) || exit 1
 }
 
 confirminputs() {
-    echo -e "\\nYou gave the following inputs:"
-    echo "Username: $username"
-    echo "Hostname: $hostname"
-    echo "Local domain: $localdomain"
-    echo "Timezone: $timezone"
-    echo "Region: $region"
-    echo "Firmware: $firmwareanswer"
-    [[ $firmwareanswer == "BIOS" ]] && echo "Device: $sdx"
-    echo "Encryption: $cryptanswer"
-    echo "Swap partition: $swapanswer"
-
-    echo
-    echo "Continue?"
-
-    select yn in "Yes" "No"; do
-        case $yn in
-            Yes ) echo "You chose 'yes'."; break;;
-            No ) echo "You chose 'no'."; echo "Exiting now."; exit 1;;
-        esac
-    done
+    whiptail --title "Confirm Your Inputs" --yes-button "Let's go!" --no-button "Never mind..." \
+        --yesno "\\nYou gave the following inputs:\\n\\n    Username: $username\\n    Hostname: $hostname\\n    Local domain: $localdomain\\n    Full address: $hostname.$localdomain\\n    Timezone: $timezone\\n    Region: $region\\n    Firmware: $firmwareanswer\\n    Device name: $sdx\\n    Encryption: $cryptanswer\\n    Swap partition: $swapanswer\\n\\nContinue?" \
+        24 85 3>&1 1>&2 2>&3 3>&1
 }
 
 adduserandpass() {
+    whiptail --infobox "Creating new user: \"$username\"" \
+        9 70
+
     echo "root:$rootpass1" | chpasswd
     unset rootpass1 rootpass2
 
-    echo -e "\\nCreating new user \"$username\"..."
     useradd -G sudo -s /bin/bash -m "$username"
     export repodir="/home/$username/.local/src"
     mkdir -p "$repodir"
@@ -237,58 +161,66 @@ adduserandpass() {
     unset userpass1 userpass2
 }
 
-setuphostname() {
-    echo "$hostname" > /etc/hostname
-    echo "127.0.0.1     localhost" > /etc/hosts
-    echo "::1           localhost" >> /etc/hosts
-    echo "127.0.1.1     $hostname $hostname.$localdomain" >> /etc/hosts
-}
-
 dobasicadjustments() {
+    whiptail --infobox "Updating packages and installing \`nala\`, a wrapper for \`apt\`..." \
+        9 70
+
     cat /proc/mounts >> /etc/fstab
     [[ $swapanswer = "yes" ]] && echo "UUID=<UUID_swap> none swap defaults 0 0" >> /etc/fstab
     blkid | grep UUID >> /etc/fstab
 
-    echo "updating packages and installing nala"
-    apt update > /dev/null 2>&1
+    echo "$hostname" > /etc/hostname
+    echo "127.0.0.1     localhost" > /etc/hosts
+    echo "::1           localhost" >> /etc/hosts
+    echo "127.0.1.1     $hostname $hostname.$localdomain" >> /etc/hosts
+
+    [[ -e /etc/localtime ]] && rm /etc/localtime
+    ln -s "/usr/share/zoneinfo/$timezone" /etc/localtime
+    hwclock --systohc
+
     apt install nala -y > /dev/null 2>&1
 }
 
-setuptime() {
-    [[ -e /etc/localtime ]] && rm /etc/localtime
-    ln -s /usr/share/zoneinfo/$timezone /etc/localtime
-    hwclock --systohc
-}
+setuplocale() {
+    whiptail --infobox "Adjusting \"/etc/locale.conf\" and \"/etc/locale.gen\"..." \
+        9 70
 
-setuplocaleconf() {
-    echo "adjusting /etc/locale.conf"
     echo "$localeconf" > /etc/locale.conf
-}
 
-setuplocalegen() {
-    echo "adjusting /etc/locale.gen"
     apt install -y locales > /dev/null 2>&1
     [[ $region == "en_US" ]] && sed -i 's/^# en_US/en_US/g' /etc/locale.gen
     locale-gen > /dev/null 2>&1
 }
 
-installpackages() {
-    echo -e "\\ninstalling Linux base packages:"
+installloop() {
+	([ -f packages.csv ] && cp packages.csv /tmp/packages.csv) && sed -i '/^#/d' /tmp/packages.csv ||
+		curl -Ls "$packfile" | sed '/^#/d' > /tmp/packages.csv
 
-    for basepkg in "${basepkgs[@]}"; do
-        echo "'$basepkg' is not yet installed on this computer. Installing '$basepkg' now..."
-        apt install -y $basepkg > /dev/null 2>&1
-    done
+	total=$(wc -l </tmp/packages.csv)
+    n="0"
 
-    echo -e "\\ninstalling additional packages:"
+	while IFS="," read -r program comment; do
+		n=$((n + 1))
+		echo "$comment" | grep -q "^\".*\"$" &&
+			comment="$(echo "$comment" | sed -E "s/(^\"|\"$)//g")"
+        install "$program" "$comment"
+	done </tmp/packages.csv
+}
 
-    for additionalpkg in "${additionalpkgs[@]}"; do
-        echo "Installing '$additionalpkg' now..."
-        apt install -y $additionalpkg > /dev/null 2>&1
-    done
+install() {
+	whiptail --title "Package Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" \
+        9 70
+	installpkg "$1"
+}
+
+installpkg() {
+	apt install -y "$1" > /dev/null 2>&1
 }
 
 doconfigs() {
+    whiptail --infobox "Performing some basic configurations. At some point, vim will open \"/etc/fstab\". You should know what to do here." \
+        9 70
+
     # clone git repos into new user's repodir
     mkdir -p \
         "/home/$username/.cache/bash" \
@@ -328,26 +260,22 @@ doconfigs() {
     ln -s "/home/$username/.dotfiles/.config/shell/aliasrc-debian" "/home/$username/.config/shell/aliasrc"
 
     chown -R "$username": "/home/$username"
-}
 
-setupfstab() {
+    systemctl enable NetworkManager
+
     sed -i '/^sysfs/,/^devpts/d' /etc/fstab
     sed -i '/^hugetlbfs/,/^binfmt_misc/d' /etc/fstab
     sed -i '/^mqueue/d' /etc/fstab
     sed -i 's/dev\/shm/tmp/g' /etc/fstab
     sed -i '/^\/dev\/sr0/d' /etc/fstab
 
-    echo
     vim /etc/fstab
-}
-
-setupnetworking() {
-    systemctl enable NetworkManager
 }
 
 docryptsetup() {
     [[ $cryptanswer = "yes" ]] && {
-        echo -e "\\ninstalling 'cryptsetup-initramfs':"
+        whiptail --infobox "Installing 'cryptsetup-initramfs'..." \
+            9 70
         apt install -y cryptsetup-initramfs
         blkid | grep UUID | grep crypto >> /etc/crypttab
         vim /etc/crypttab
@@ -355,30 +283,38 @@ docryptsetup() {
 }
 
 doinitramfsupdate() {
-    echo -e "\\nupdating initramfs"
+    whiptail --infobox "Updating initramfs..." \
+        9 70
     update-initramfs -u -k all > /dev/null 2>&1
 }
 
 dogrubinstall() {
-    echo "installing GRUB"
+    whiptail --infobox "Installing and updating GRUB..." \
+        9 70
 
     [[ $firmwareanswer = "BIOS" ]] && {
         apt install -y grub-pc > /dev/null 2>&1
-        grub-install --target=i386-pc /dev/$sdx
+        grub-install --target=i386-pc "/dev/$sdx" > /dev/null 2>&1
     }
 
     [[ $firmwareanswer = "UEFI" ]] && {
         apt install -y grub-efi > /dev/null 2>&1
-        grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+        grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB > /dev/null 2>&1
     }
 
-    echo "updating GRUB"
     update-grub > /dev/null 2>&1
+}
+
+finalmessage() {
+	whiptail --title "Congrats!" --msgbox "Provided there were no hidden errors, \`debian-setup\` completed successfully and all packages and configurations are properly installed." \
+        9 70
 }
 
 ################################
 # ACTUAL SCRIPT
 ################################
+
+echo "Updating packages, one moment..." && apt update > /dev/null 2>&1 && installpkg whiptail > /dev/null 2>&1
 
 getuserandpass
 
@@ -390,26 +326,18 @@ confirminputs
 
 adduserandpass
 
-setuphostname
-
 dobasicadjustments
 
-setuptime
+setuplocale
 
-setuplocaleconf
-
-setuplocalegen
-
-installpackages
+installloop
 
 doconfigs
-
-setupfstab
-
-setupnetworking
 
 docryptsetup
 
 doinitramfsupdate
 
 dogrubinstall
+
+finalmessage
